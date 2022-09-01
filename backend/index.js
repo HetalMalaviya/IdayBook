8c8005c9aec0407ce6bba12091f918bb9cc3d5bb
Hetal Malaviya <hetalmalaviya71@gmail.com>
2022-09-01 15:27:48 +0200

    connect to database

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const connecttoMongo = require("./db");
+const express = require("express");
+
+connecttoMongo();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
