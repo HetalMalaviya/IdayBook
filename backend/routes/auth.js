4d58c14ea775b01e8672cd8b8d942fdd360b22f4
Hetal Malaviya <hetalmalaviya71@gmail.com>
2022-09-02 09:38:06 +0200

    add authentication using express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  const user = User(req.body);
-  user.save();
-  res.send("Hello");
-});
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 5 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+      async function someFunc() {
+        let user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+          return res
+            .status(400)
+            .json({ error: "sorry email is already exits" });
+        }
+        user = await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        // .then((user) => res.json(user))
+        // .catch((err) => {
+        //   console.log(err);
+        //   res.json({ error: "please enter valid data", message: err.message });
+        // });
+        res.json(user);
+      }
+
+      someFunc();
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("error occured");
+    }
+  }
+);
 
 module.exports = router;
