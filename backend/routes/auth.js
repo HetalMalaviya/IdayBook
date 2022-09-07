743a5a8be6549c36fcd6c436b242fb181d7142cd
Hetal Malaviya <hetalmalaviya71@gmail.com>
2022-09-07 11:18:55 +0200

    add authentication using jwt tokenr

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { body, validationResult } = require("express-validator");
+const fetchuser = require("../middleware/Fetchuser");
 
+const JWT_SECRET = "Iamdeveloper$";
 router.post(
   "/createuser",
   [
@@ -10,39 +14,84 @@ router.post(
     body("email").isEmail(),
     body("password").isLength({ min: 8 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      async function someFunc() {
-        let user = await User.findOne({ email: req.body.email });
+      let user = await User.findOne({ email: req.body.email });
 
-        if (user) {
-          return res
-            .status(400)
-            .json({ error: "sorry email is already exits" });
-        }
-        user = await User.create({
-          name: req.body.name,
-          email: req.body.email,
-          password: req.body.password,
-        });
-        // .then((user) => res.json(user))
-        // .catch((err) => {
-        //   console.log(err);
-        //   res.json({ error: "please enter valid data", message: err.message });
-        // });
-        res.json(user);
+      if (user) {
+        return res.status(400).json({ error: "sorry email is already exits" });
       }
+      const salt = await bcrypt.genSalt(10);
 
-      someFunc();
+      const secPass = await bcrypt.hash(req.body.password, salt);
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const jwttoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ jwttoken });
     } catch (error) {
       console.log(error.message);
       res.status(500).send("error occured");
     }
   }
 );
+router.post(
+  "/login",
+  [body("email").isEmail(), body("password").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ error: "please login with right value" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res.status(400).json({ error: "please login with right value" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const jwttoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ jwttoken });
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send("internal server error");
+    }
+  }
+);
+
+router.post("/getuser", fetchuser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("internal server error");
+  }
+});
 module.exports = router;
